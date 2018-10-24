e5078255df842317d12e2dced9d41cbc413579df
Jeppe Rasmussen <jerasmu@microsoft.com>
2018-10-24 16:22:33 +0200

    change server.js

--- server.js
+++ server.js
@@ -48,4 +48,13 @@ app.use(function(err, req, res, next) {
   res.status(422).send({ error: err.message });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
